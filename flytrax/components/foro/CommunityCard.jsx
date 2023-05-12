2e8000642d597fd72ee895fa7b3889f465ac24ba
Guille Cánovas <755848@unizar.es>
2023-05-12 13:42:02 +0200

    Update CommunityCard.jsx

--- flytrax/components/foro/CommunityCard.jsx
+++ flytrax/components/foro/CommunityCard.jsx
@@ -15,8 +15,8 @@ const CommunityCard = ({ aeropuertos }) => {
             <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1  gap-6">
                 
                 {slice.map((airport, index) => (
-                    <Link href={`/community/${airport.iata_code}`}> 
-                        <div key={index} className= "border-b-[1px] border-neutral-800 p-5 cursor-pointer hover:bg-neutral-200 transition bg-neutral-100 text-black shadow-lg  hover:scale-110 rounded-lg transform duration-500 relative my-1">
+                    <Link key={index} href={`/community/${airport.iata_code}`}> 
+                        <div className= "border-b-[1px] border-neutral-800 p-5 cursor-pointer hover:bg-neutral-200 transition bg-neutral-100 text-black shadow-lg  hover:scale-110 rounded-lg transform duration-500 relative my-1">
                             <div className="flex flex-row items-start gap-3">
                                 <div className="flex flex-row items-start gap-3">
                                     <p className="font-semibold cursor-pointer hover:underline"> {airport.iata_code} </p>
